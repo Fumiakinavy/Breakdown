f4d4e70dd5da983d5ce1ffcea361513c9141b34e
Your Name <you@example.com>
2025-10-21 17:00:48 +0900

    Initial Commit

---
+++ Breakdown/BreakdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakdownApp.swift
+//  Breakdown
+//
+//  Created by uehara fumiaki on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BreakdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
